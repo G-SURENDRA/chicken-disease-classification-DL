58cd0c9194f55c20bd2287fcb95711602c609cf8
G-SURENDRA <surendragara999@gmail.com>
2025-09-11 15:45:16 +0530

    update changes

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: [%(message)s]:')
 
-project_name="chicken-disease_classification"
+project_name="cnnClassifier"
 
 list_of_files=[
     "github/workflows/.gitkeep",
@@ -22,6 +22,7 @@ list_of_files=[
     "requirements.txt",
     "setup.py",
     "research/trails.ibynp",
+    "templates/index.html"
 ]
 
 
